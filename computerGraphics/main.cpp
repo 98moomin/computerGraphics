f1c266541022ecc0b762dadc2715dc24107efded
98moomin <moomin9805@gmail.com>
2022-03-31 14:42:55 +0900

    Initial Commit

---
+++ computerGraphics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  computerGraphics
+//
+//  Created by moomin on 2022/03/31.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
